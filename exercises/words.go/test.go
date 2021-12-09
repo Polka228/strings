43f40a10f1482161687a2985d6b2776d9a91ca65
Polka228 <jefremovapolina@gmail.com>
2021-12-09 10:39:29 +0200

    ,

--- exercises/words.go/test.go
+++ exercises/words.go/test.go
@@ -2,9 +2,75 @@ package main
 
 import (
 	"fmt"
+	"unicode"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func IsSep(r rune) bool { return unicode.IsSpace(r) || unicode.IsPunct(r) || unicode.IsSymbol(r) }
+
+func countWords(s string) (cnt int) {
+	wasSep := true
+	for _, r := range s {
+		if IsSep(r) {
+			wasSep = true
+		} else if wasSep {
+			wasSep = false
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func TestCountWords() {
+	pass := true
+	for _, tc := range []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{"empty", "", 0},
+		{"separators", " *-+!,.\n\r\t", 0},
+		{"single word", "abc", 1},
+		{"single word in spaces", "   abc   ", 1},
+		{"two words", "abc def", 2},
+		{"two words in spaces", "   abc   def   ", 2},
+		{"unicode", "привет, мир!", 2},
+	} {
+		if got := countWords(tc.input); got != tc.want {
+			fmt.Printf("%s\n\tFAIL: got %v, want %v\n", tc.name, got, tc.want)
+			pass = false
+		}
+	}
+	if pass {
+		fmt.Println("ALL PASS")
+	}
+}
+
+func words(s string) []string {
+	wasSep := true
+	var w []string
+	var tmp []rune
+	for _, r := range s {
+		if IsSep(r) {
+			if !wasSep {
+				w = append(w, string(tmp))
+				// tmp = tmp[:0]
+				tmp = nil
+			}
+			wasSep = true
+		} else {
+			tmp = append(tmp, r)
+			wasSep = false
+		}
+
+	}
+	if len(tmp) > 0 {
+		w = append(w, string(tmp))
+	}
+
+	return w
+}
 
 func TestWords() {
 	pass := true
@@ -30,4 +96,10 @@ func TestWords() {
 	if pass {
 		fmt.Println("ALL PASS")
 	}
+}
+
+func main() {
+	TestCountWords()
+	TestWords()
+
 }
